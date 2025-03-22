52c7513c6a6e324f381c5fdfb8495123bed72db1
leinadalrus <daniel.david.surla@gmail.com>
2025-03-22 09:16:18 +0800

    created new components and removed former components

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,7 @@
+<script setup>
+import TradingCard from './TradingCard.vue'
+</script>
+
+<template>
+    <TradingCard />
+</template>
---
+++ src/components/TradingCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    flavour: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        url: String,
+        alt: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <article id="card-base" class="">
+        <img v-bind:src="image.url" v-bind:alt="image.alt" class="" />
+
+        <h1 class="">{{ title }}</h1>
+
+        <div id="card-content" class="">
+            <p class="">{{ description }}</p>
+            <i class="">{{ flavour }}</i>
+        </div>
+    </article>
+</template>
+
+<style scoped>
+#card-base {
+    margin-top: 1rem;
+    display: flex;
+    position: relative;
+}
+
+#card-content {
+    flex: 1;
+    margin-left: 1rem;
+}
+
+h1 {
+    font-size: 1.1rem;
+    font-weight: 300;
+    margin-bottom: 0.5rem;
+}
+
+i {
+    display: flex;
+    place-items: center;
+    place-content: center;
+    width: 2rem;
+    height: 3rem;
+}
+
+@media (min-width: 8rem) {
+    #card-base {
+        margin-top: 0;
+        padding: 0.4rem 0 1rem 1rem;
+    }
+}
+</style>
